3d42b420c4aa93e46e6e076091e1f5eb02b55664
Tanguy BAYART <tanguy.bayart@gmail.com>
2021-04-18 18:14:06 +0200

    v0.59.17

--- deployEnjoySport.sh
+++ deployEnjoySport.sh
@@ -14,7 +14,7 @@ dateD=`date +"%Y-%m-%d à %H:%M"`
 VERSION=`grep version package.json  | cut -d "\"" -f4`
 
 echo "### deployEnjoySport.sh - `date` - date de déploiement : " $dateD
-echo "### deployEnjoySport.sh - `date` - date de déploiement : version =" $1
+echo "### deployEnjoySport.sh - `date` - date de déploiement : version =" $VERSION
 
 sed "s/==dateyyyymmddHHmmss==/${dateD}/g" /home/ec2-user/enjoySportRepo/src/app/welcome-view/welcome-view.component.html >| /home/ec2-user/enjoySportRepo/src/app/welcome-view/welcome-view.component.html.tmp1
 sed "s/==versionDev==/$VERSION/g" /home/ec2-user/enjoySportRepo/src/app/welcome-view/welcome-view.component.html.tmp1 >| /home/ec2-user/enjoySportRepo/src/app/welcome-view/welcome-view.component.html.tmp2
