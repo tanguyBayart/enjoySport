7ae14c044194c42293c493447918f9f75a800b54
Tanguy <tanguy.bayart@gmail.com>
2022-10-25 17:27:25 +0200

    big fix : deployEnjoySport

--- deployEnjoySport.sh
+++ deployEnjoySport.sh
@@ -34,7 +34,7 @@ ls -l /home/iaam4644/EJ2repo/enjoySport/src/app/welcome-view/welcome-view.compon
 
 # build
 echo "### deployEnjoySport.sh - `date` - Génération du livrable"
-/home/iaam4644/EJ2repo/enjoySport/node_modules/.bin/ng
+/home/iaam4644/EJ2repo/enjoySport/node_modules/.bin/ng build
 
 if [[ $? -ne 0 ]]
 then
